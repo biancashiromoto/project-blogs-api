1fee06cccb8ddfea1dbe9b17facc11b428b69e9b
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-25 19:42:40 -0300

    feat: Create PostCategory model

---
+++ src/models/PostCategory.js
@@ -0,0 +1,51 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize')} Sequelize 
+   */
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'blog_posts',
+        key: 'id'
+      },
+      primaryKey: true
+      // field: 'post_id'
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'categories',
+        key: 'id'
+      },
+      primaryKey: true
+      // field: 'category_id'
+    }
+  }, {
+    timestamps: false,
+    underscored: true,
+    tableName: 'posts_categories'
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategory,
+      as: 'post_id',
+      foreignKey: 'postId',
+      otherKey: 'categoryId'
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategory,
+      as: 'category_id',
+      foreignKey: 'categoryId',
+      otherKey: 'postId'
+    });
+  }
+
+  return PostCategory;
+};
