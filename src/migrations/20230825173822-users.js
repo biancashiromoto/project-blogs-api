2a0aee8fbb13b37f0a5ce85db67c1fa9c2d275a8
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-25 16:21:53 -0300

    feat: Create migrations

---
+++ src/migrations/20230825173822-users.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      display_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      }
+    }, {
+      timestamps: false,
+      underscored: true,
+      tableName: 'users'
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ src/migrations/20230825173905-posts_categories.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {*} Sequelize 
+   */
+  up: async (queryInterface, Sequelize) => {
+    
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
