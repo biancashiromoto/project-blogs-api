62751209c06baf8817b4bcca919b0ff74c6ab9e2
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-25 18:59:58 -0300

    feat: Create BlogPost model

---
+++ src/models/BlogPost.js
@@ -0,0 +1,46 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    title: {
+      type: DataTypes.STRING
+    },
+    content: {
+      type: DataTypes.STRING
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id'
+      },
+      field: 'user_id'
+    },
+    published: {
+      type: DataTypes.DATE
+    },
+    updated: {
+      type: DataTypes.DATE
+    }
+  }, {
+    timestamps: false,
+    underscored: true,
+    tableName: 'blog_posts'
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      as: 'User'
+    })
+  }
+
+  return BlogPost;
+};
