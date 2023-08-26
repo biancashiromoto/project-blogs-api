307d19e056800d06c9b704129229d1fa9fc15b61
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-26 14:51:32 -0300

    feat: Create registerUser

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,10 +1,26 @@
-const { User } = require('../models');
+const { User, sequelize } = require('../models');
+const { userSchema } = require('./validations/schemas');
 
 const findAll = async () => {
   const users = await User.findAll();
   return users;
 };
 
+const registerUser = async (user) => {
+  const { error, value } = userSchema.validate(user);
+  if (error) {
+    return { status: 400, data: { message: error.message } };
+  }
+
+  const result = await sequelize.transaction(async (transaction) => {
+    const newUser = await User.create(value, { transaction });
+    return { status: 201, data: newUser.dataValues };
+  });
+
+  return result;
+};
+
 module.exports = {
   findAll,
+  registerUser,
 };
