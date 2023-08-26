e0915c0451346f0d50e525ad69cef83c6ccc0f06
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-25 21:21:22 -0300

    feat: Add POST request to /login

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { User, Category } = require('./models');
-// ...
+const { login } = require('./controllers');
 
 const app = express();
 
@@ -20,6 +20,9 @@ app.get('/categories', async (req, res) => {
   const categories = await Category.findAll();
   res.json(categories);
 });
+
+console.log(login);
+app.post('/login', login);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
