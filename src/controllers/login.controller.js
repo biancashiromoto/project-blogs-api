21ad65fdfe912c43e2ab1bcca916af8d5dd53c99
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-25 23:23:13 -0300

    feat: Add login validation and token generation

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,13 +1,15 @@
-// const Joi = require('joi');
-const jwt = require('jsonwebtoken');
 const { loginService } = require('../services');
-
-const { JWT_SECRET } = process.env;
 
 const loginController = async (req, res) => {
   const { email, password } = req.body;
-  await loginService(email, password);
-  res.send('HELLOOOO');
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Some required fields are missing' });
+  }
+  const { status, data } = await loginService(email, password);
+  if (status === 'UNAUTHENTICATED') {
+    return res.status(400).json(data);
+  }
+  res.status(200).json(data);
 };
 
 module.exports = { loginController };
--- src/services/login.service.js
+++ src/services/login.service.js
@@ -1,5 +1,32 @@
-const loginService = (email, password) => ({
-    token: 'token',
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+const { JWT_SECRET } = process.env;
+
+const unauthenticated = {
+  status: 'UNAUTHENTICATED',
+  data: { message: 'Invalid fields' },
+};
+
+const loginService = async (email, password) => {
+  const user = await User.findOne({ where: { email } });
+  console.log(user);
+  if (!user || user.password !== password) {
+    return unauthenticated;
+  }
+  const jwtPayload = {
+    sub: user.id,
+  };
+
+  const token = jwt.sign(jwtPayload, JWT_SECRET, {
+    algorithm: 'HS256',
+    expiresIn: '7d',
   });
+
+  return ({
+    status: 'SUCCESS',
+    data: { token },
+  });
+};
 
 module.exports = loginService;
