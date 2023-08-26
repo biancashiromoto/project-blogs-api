e571e96e9d163e5ecec73713974f731b21dcba92
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-26 14:22:45 -0300

    feat: Create findAll service

---
+++ src/services/categories.service.js
@@ -0,0 +1,10 @@
+const { Category } = require('../models');
+
+const findAll = async () => {
+  const categories = await Category.findAll();
+  return categories;
+};
+
+module.exports = {
+  findAll,
+};
