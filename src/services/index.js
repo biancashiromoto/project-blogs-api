d8414d7c568669837bdc66454cccd5db307df6c8
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-28 15:38:19 -0300

    feat: Export postsService

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const loginService = require('./login.service');
 const userService = require('./user.service');
 const categoriesService = require('./categories.service');
+const postsService = require('./posts.service');
 
 module.exports = {
   loginService,
   userService,
   categoriesService,
+  postsService,
 };
