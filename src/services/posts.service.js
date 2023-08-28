3cc3d19e5526d9626782f9bccf5040fbc6d50f63
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-28 16:36:25 -0300

    feat: Add relation to User model

--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -1,7 +1,16 @@
-const { BlogPost } = require('../models');
+const { BlogPost, User } = require('../models');
 
 const findAll = async () => {
-  const posts = await BlogPost.findAll();
+  const posts = await BlogPost.findAll(
+    { 
+      attributes: { exclude: ['user_id'] },
+      include: {
+        model: User,
+        as: 'user',
+        attributes: ['id', 'displayName', 'email', 'image'],
+      },
+    },
+  );
   return posts;
 };
 
