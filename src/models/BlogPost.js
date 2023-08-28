ffa382dba476d9365dcbd6fdc702e4296fd00772
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-28 16:35:06 -0300

    fix: Fix alias for BlogPost association

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -38,7 +38,7 @@ module.exports = (sequelize, DataTypes) => {
   BlogPost.associate = (models) => {
     BlogPost.belongsTo(models.User, {
       foreignKey: 'user_id',
-      as: 'User'
+      as: 'user'
     })
   }
 
