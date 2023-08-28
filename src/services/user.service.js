a5b2fdbb937a99815dfd0e235e5653873f013c93
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-28 18:53:56 -0300

    feat: Create function findById

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -32,7 +32,17 @@ const registerUser = async (user) => {
   return result;
 };
 
+const findById = async (id) => {
+  const user = await User.findByPk(id, {
+    attributes: { exclude: ['password'] },
+  });
+
+  if (!user) return { status: 404, data: { message: 'User does not exist' } };
+  return { status: 200, data: user };
+};
+
 module.exports = {
   findAll,
   registerUser,
+  findById,
 };
