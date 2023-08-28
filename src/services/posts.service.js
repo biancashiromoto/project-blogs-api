dd1ea9174daacbed12475ea3e5bcb19edb8f6fd7
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-28 19:02:03 -0300

    feat: Create function findById

--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -21,6 +21,29 @@ const findAll = async () => {
   return { status: 200, data: posts };
 };
 
+const findById = async (id) => {
+  const post = await BlogPost.findByPk(id, {
+    include: [
+      {
+        model: User,
+        as: 'user',
+        attributes: ['id', 'displayName', 'email', 'image'],
+      },
+      {
+        model: Category,
+        as: 'categories',
+        attributes: ['id', 'name'],
+        through: { attributes: [] },
+      },
+    ],
+  });
+
+  if (!post) return { status: 404, data: { message: 'Post does not exist' } };
+  
+  return { status: 200, data: post };
+};
+
 module.exports = {
   findAll,
+  findById,
 };
